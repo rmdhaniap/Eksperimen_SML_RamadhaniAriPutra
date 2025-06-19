952f32e78dce2b58dc2c6ba3bb61774f5f761a26
Ramadhani Ari Putra <127958544+rmdhaniap@users.noreply.github.com>
2025-06-19 17:46:51 +0700

    Update automate_RamadhaniAri.py

--- preprocessing/automate_RamadhaniAri.py
+++ preprocessing/automate_RamadhaniAri.py
@@ -37,4 +37,4 @@ os.makedirs('preprocessing', exist_ok=True)
 # Simpan hasil preprocessing ke folder
 df.to_csv('diabetes_preprocessing.csv', index=False)
 
-print("✅ Preprocessing selesai. File disimpan di 'preprocessing/obesity_data_preprocessing.csv'")
+print("✅ Preprocessing selesai. Dataset Berhasil Disimpan'")
